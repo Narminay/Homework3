e9ec72a8d01129f761d2bcbf15479fe940d84fb1
Narminay <narminay@code.edu.az>
2022-11-14 03:07:03 +0400

    Homework3

---
+++ project3/Program.cs
@@ -0,0 +1,109 @@
+﻿#region Task 1: 2-nin qüvvəti
+//Console.Write("Quvvet olmasi yoxlanan reqem daxil et: ");
+//string numStr = Console.ReadLine();
+//int num = Convert.ToInt32(numStr);
+//Console.WriteLine("Yoxlanan reqem:" + num);
+
+//int y = 1;
+
+//for (int i = 1; i <= num; i *= 2)
+//{
+//    y = i;
+//}
+
+//Console.Write("Daxil etdiyin reqem: ");
+//if (y == num)
+//{
+//    Console.WriteLine( "Eded 2-nin quvvetidir!");
+//}
+//else
+//{
+//    Console.WriteLine("Eded 2-nin quvveti deyil!");
+//}
+#endregion
+
+#region Task 2: Iki rəqəm arasındaki fərq
+//Console.WriteLine("Ilk Rəqəmi seçin :");
+//int reqem1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ikinci Rəqəmi seçin :");
+//int reqem2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Iki ədəd arası :");
+//int f = 0;
+
+//for (int i = reqem1; i <= reqem2; i++)
+//{
+//    if (reqem2 < reqem1) ;
+//    {
+//        f = i;
+//        Console.WriteLine(f);
+//    }
+//}
+//for (int i = reqem1; i >= reqem2; i--)
+//{
+//    Console.WriteLine(i);
+//}
+#endregion
+
+#region Task 3: 70 dən böyük/kiçik məsələsi))) 
+//Console.WriteLine("Ilk eded :");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int c = 70;
+//int count = 3;
+//if (n > c)
+//{
+//    n -= c;
+//    count *= n;
+//    Console.WriteLine(count);
+//}
+//else
+//{
+//    c -= n;
+//    Console.WriteLine(c);
+//}
+#endregion
+
+#region Task 4: X,Y,Z
+//Console.WriteLine("X-i seçin:");
+//int X = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Y-i seçin:");
+//int Y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Z-i seçin:");
+//int Z = Convert.ToInt32(Console.ReadLine());
+//if (X > Y)
+//{
+//    if (X > Z)
+//    {
+//        Console.Write("X boyukdur.");
+//    }
+//    else
+//    {
+//        Console.Write("Z boyukdur.");
+//    }
+//}
+//else if (Y > Z)
+//{
+//    Console.Write("Y boyukdur.");
+//}
+//else
+//{
+//    Console.Write("Z boyukdur.");
+//}
+#endregion
+
+#region n və m (n<m) ədədləri arasında neçə tək ədəd var
+//Console.WriteLine("n-i seçin:");
+//int n = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("m-i seçin:");
+//int m = Convert.ToInt32(Console.ReadLine());
+//int count = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 != 0)
+//    {
+//        count++;
+//        Console.WriteLine(i);
+//    }
+//}
+//Console.WriteLine("Təklərin sayı: " + count);
+#endregion
